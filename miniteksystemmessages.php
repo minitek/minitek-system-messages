c33db2054924e99aa359c82e6eca902210a5d8a0
Minitek <info@minitek.gr>
2021-06-16 17:05:09 +0300

    Added parameters for customizing colors

--- miniteksystemmessages.php
+++ miniteksystemmessages.php
@@ -126,6 +126,60 @@ class plgSystemMinitekSystemMessages extends JPlugin
 		$easing = $this->params->get('easing', 'linear');
 		$effectDuration = $this->params->get('effectDuration', 250);
 
+		// Colors 
+		$message_color = $this->params->get('message_color', '#0ec47d');
+		$message_text = $this->params->get('message_text', '#ffffff');
+		$info_color = $this->params->get('info_color', '#00b1fe');
+		$info_text = $this->params->get('info_text', '#ffffff');
+		$warning_color = $this->params->get('warning_color', '#ffc107');
+		$warning_text = $this->params->get('warning_text', '#555555');
+		$error_color = $this->params->get('error_color', '#f76860');
+		$error_text = $this->params->get('error_text', '#ffffff');
+		$css = '';
+
+		if ($theme === 'default' || $theme === 'compact') {
+			$css .= '
+			.polipop_theme_default .polipop__notification_type_success,
+			.polipop_theme_compact .polipop__notification_type_success {
+				background-color: ' . $message_color . ';
+				color: ' . $message_text . ';
+			}
+			.polipop_theme_default .polipop__notification_type_success .polipop__notification-icon svg,
+			.polipop_theme_compact .polipop__notification_type_success .polipop__notification-icon svg {
+				fill: ' . $message_text . ';
+			}
+			.polipop_theme_default .polipop__notification_type_info,
+			.polipop_theme_compact .polipop__notification_type_info {
+				background-color: ' . $info_color . ';
+				color: ' . $info_text . ';
+			}
+			.polipop_theme_default .polipop__notification_type_info .polipop__notification-icon svg,
+			.polipop_theme_compact .polipop__notification_type_info .polipop__notification-icon svg {
+				fill: ' . $info_text . ';
+			}
+			.polipop_theme_default .polipop__notification_type_warning,
+			.polipop_theme_compact .polipop__notification_type_warning {
+				background-color: ' . $warning_color . ';
+				color: ' . $warning_text . ';
+			}
+			.polipop_theme_default .polipop__notification_type_warning .polipop__notification-icon svg,
+			.polipop_theme_compact .polipop__notification_type_warning .polipop__notification-icon svg {
+				fill: ' . $warning_text . ';
+			}
+			.polipop_theme_default .polipop__notification_type_error,
+			.polipop_theme_compact .polipop__notification_type_error {
+				background-color: ' . $error_color . ';
+				color: ' . $error_text . ';
+			}
+			.polipop_theme_default .polipop__notification_type_error .polipop__notification-icon svg,
+			.polipop_theme_compact .polipop__notification_type_error .polipop__notification-icon svg {
+				fill: ' . $error_text . ';
+			}
+			';
+		}
+
+		$document->addStyleDeclaration($css);
+
 		// Load Polipop
 		$wa->registerAndUseStyle('plg_system_miniteksystemmessages.polipop.core', 'plg_system_miniteksystemmessages/polipop.core.css');
 
